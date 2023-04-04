ad2294487cea61efee0c993d63c244b8ecabad24
AllVoll <128129005+AllVoll@users.noreply.github.com>
2023-04-04 09:58:59 +0300

    Delete api-db directory

--- api-db/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Выполнение миграций Alembic перед запуском приложения
-alembic revision --autogenerate -m "Add primary key to ApiKey"
-alembic upgrade head
-
-# Запуск приложения
-exec "$@"
